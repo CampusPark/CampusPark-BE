46d4554036eedfe084194a3b45eb1f6a958c589d
Hyun <hyun_7586@naver.com>
2025-09-07 19:07:30 +0900

    init: 초기 세팅 및 이슈/PR 템플릿 생성

---
+++ src/main/java/org/example/campuspark/CampusParkApplication.java
@@ -0,0 +1,13 @@
+package org.example.campuspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/campuspark/CampusParkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campuspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class CampusParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
