797a37a03401234a763a2dee96509e036ffd84b0
Hyun <hyun_7586@naver.com>
2025-09-13 15:40:08 +0900

    refactor: Application.java에 ConfigurationPropertiesScan 어노테이션 추가

--- src/main/java/org/example/campuspark/CampusParkApplication.java
+++ src/main/java/org/example/campuspark/CampusParkApplication.java
@@ -2,10 +2,12 @@ package org.example.campuspark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CampusParkApplication {
 
     public static void main(String[] args) {
