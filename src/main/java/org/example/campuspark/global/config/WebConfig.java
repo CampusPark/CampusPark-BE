d251f8c41c17f6dff1570f88eeaa128a6a165e26
jbh010204 <jbh010204@naver.com>
2025-09-13 23:24:11 +0900

    feat: Add Netlify app URL to CORS allowed origins

--- src/main/java/org/example/campuspark/global/config/WebConfig.java
+++ src/main/java/org/example/campuspark/global/config/WebConfig.java
@@ -11,7 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000", "http://localhost:5174",
-                "http://localhost:8080", "http://moamoa-k.s3-website.ap-northeast-2.amazonaws.com")
+                "http://localhost:8080",
+                "http://moamoa-k.s3-website.ap-northeast-2.amazonaws.com",
+                "https://luminous-melba-20e1e6.netlify.app"
+            )
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
