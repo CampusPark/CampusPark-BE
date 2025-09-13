0cc14f0057687367e9150755417f6d1a3bc66345
Hyun <hyun_7586@naver.com>
2025-09-13 20:44:57 +0900

    refactor: CORS 설정

---
+++ src/main/java/org/example/campuspark/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.example.campuspark.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://localhost:5174",
+                "http://localhost:8080", "http://moamoa-k.s3-website.ap-northeast-2.amazonaws.com")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
